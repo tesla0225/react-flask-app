f1115aaba1b21daf9e8f221f66e0f059e0d34556
tesla0225 <moyasin.tech@gmail.com>
2022-03-04 16:06:51 +0900

    message

--- api/api.py
+++ api/api.py
@@ -1,8 +1,13 @@
 import time
-from flask import Flask
 
+from flask import Flask, render_template,  request, url_for, session , redirect
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
+SLACK_ACCESS_TOKEN ='xoxb-3083027697062-3161652459141-5j9JbmA07psLrTSpuLQGeGMs'
+# Slackクライアントを作成
+client = WebClient(SLACK_ACCESS_TOKEN)
 
 @app.errorhandler(404)
 def not_found(e):
@@ -17,3 +22,27 @@ def index():
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/api/history', methods=["GET", "POST"])
+def get_chathistory():
+    try:
+        response = client.conversations_replies(channel='C032ML8QYDQ',ts=request.form["ts"])
+        return {"data":response["messages"]}
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        return (f"Got an error: {e.response['error']}")
+
+@app.route('/api/chat', methods=["GET", "POST"])
+def get_chatstart():
+    try:
+        response = client.chat_postMessage(channel='#random', text=request.form["text"],thread_ts=request.form["ts"])
+       # assert response["message"]["text"] == request.form["text"]
+        return response["ts"]
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        return (f"Got an error: {e.response['error']}")
+
--- src/App.js
+++ src/App.js
@@ -1,44 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import history from "./history";
+import "./App.css";
+import axios from "axios";
+let url = new URL(window.location.href);
+let urlParam = new URLSearchParams(url.search.slice(1));
+let message = [];
+
+function getHistory(ts) {
+  if (urlParam.has("ts")) {
+    axios.post("/api/history", ts).then((res) => {
+      console.log("受け取り");
+      console.log(res.data.data);
+      message = res.data.data;
+      messagesList(message);
+      return res.data.data;
+    });
+  }
+}
+
+function messagesList(list) {
+  let chat = document.getElementById("chat");
+  let chatHTML = "";
+  console.log(list);
+
+  let chatList = list?.map((item, index) => {
+    let username = "";
+    let icon = "";
+    if ("bot_id" in item) {
+      username = "bot";
+      icon = item.bot_profile.icons.image_72;
+    } else {
+      username = "user";
+      icon = "https://placehold.jp/150x150.png";
+    }
+
+    chatHTML += `<li class="chat-item">
+    <span class="chat-item_name">${username}</span>
+    <img class="chat-item_icon" src="${icon}">
+    <p class="chat-item_text">${item.text}</p>
+    </li>`;
+  });
+  chat.innerHTML = chatHTML;
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [messages, setMessages] = useState({ data: [] });
+  const inputRef = useRef();
+  const scrollBottomRef = useRef();
+  url = new URL(window.location.href);
+  urlParam = new URLSearchParams(url.search.slice(1));
+  getHistory(urlParam);
 
   useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+    if (scrollBottomRef && scrollBottomRef.current) {
+      scrollBottomRef.current.scrollIntoView();
+    }
+  }, [messages]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(messagesList);
+
+    url = new URL(window.location.href);
+    urlParam = new URLSearchParams(url.search.slice(1));
+    getHistory(urlParam);
+
+    // multipart/form-data形式のデータをapplication/x-www-form-urlencodedに変換
+    const params = new URLSearchParams(new FormData(event.currentTarget));
+    params.append("ts", urlParam);
+    axios.post("/api/chat", params).then((res) => {
+      if (!urlParam.has("ts")) {
+        history.push("/?ts=" + res.data);
+      }
     });
-  }, []);
+
+    inputRef.current.value = "";
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-          <div>
-            <Link className="App-link" to="/">Home</Link>
-            &nbsp;|&nbsp;
-            <Link className="App-link" to="/page2">Page2</Link>
-          </div>
           <Switch>
             <Route exact path="/">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-                <p>The current time is {currentTime}.</p>
+              <div className="container">
+                <ul className="chat" id="chat"></ul>
+                <form onSubmit={(event) => handleSubmit(event)}>
+                  <input
+                    name="text"
+                    ref={inputRef}
+                    type="text"
+                    placeholder="ここに文字を入れる"
+                  ></input>
+                  <button ref={scrollBottomRef} type="submit">
+                    送信
+                  </button>
+                </form>
+              </div>
             </Route>
             <Route path="/page2">
-                <p>This is page 2!</p>
+              <p>This is page 2!</p>
             </Route>
           </Switch>
         </BrowserRouter>
